850fe56d958a00a2cc2a1d5522798f4c2b0b0ab8
Michal Parusinski <michal@parusinski.me>
2022-11-24 18:20:32 +0100

    Disable kitty bell

--- home/programs/kitty.nix
+++ home/programs/kitty.nix
@@ -5,6 +5,7 @@
     enable = true;
     settings = {
       background_opacity = "0.9";
+      enable_audio_bell = false;
       font_size = "12.0";
       font_family = "xft:Jet Brains Mono";
       # Black
