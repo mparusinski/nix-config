22c5b002482b64afde6fa95dfaa7a435bf95a4fa
Michal Parusinski <michal@parusinski.me>
2023-11-12 15:43:36 +0100

    Integrating home-manager to the system

--- build_home.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-home-manager switch -f home/home.nix
--- flake.nix
+++ flake.nix
@@ -1,8 +1,24 @@
 {
   description = "Michal's NixOS configuration";
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
-  outputs = { self, nixpkgs, ... }@inputs : {
+  # This is the standard format for flake.nix.
+  # `inputs` are the dependencies of the flake,
+  # and `outputs` function will return all the build results of the flake.
+  # Each item in `inputs` will be passed as a parameter to
+  # the `outputs` function after being pulled and built.
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      # The `follows` keyword in inputs is used for inheritance.
+      # Here, `inputs.nixpkgs` of home-manager is kept consistent with
+      # the `inputs.nixpkgs` of the current flake,
+      # to avoid problems caused by different versions of nixpkgs.
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, home-manager,... }@inputs : {
       nixosConfigurations = {
         # Main personal laptop (thor)
         thor = nixpkgs.lib.nixosSystem {
@@ -46,7 +62,15 @@
           # you must use `specialArgs` by uncomment the following line:
           #
           # specialArgs = {...};  # pass custom arguments into all sub module.
-          modules = [ ./system/thor/configuration.nix ];
+          modules = [ 
+            ./system/thor/configuration.nix
+            home-manager.nixosModules.home-manager
+            {
+              home-manager.useGlobalPkgs = true;
+              home-manager.useUserPackages = true;
+              home-manager.users.michalparusinski = import ./home/home.nix;
+            }
+          ];
         };
       };
   };
