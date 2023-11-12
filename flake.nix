7256aa35db91b98fddcc8e16c3a41404ebeeb18d
Michal Parusinski <michal@parusinski.me>
2023-11-12 16:39:57 +0100

    Added cache mirrors

--- flake.nix
+++ flake.nix
@@ -1,6 +1,19 @@
 {
   description = "Michal's NixOS configuration";
 
+  # Setting up cache servers
+  nixConfig = {
+    substituters = [
+      "https://cache.nixos.org/"
+    ];
+    extra-substituters = [
+      "https://nix-community.cachix.org"
+    ];
+    extra-trusted-public-keys = [
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+    ];
+  };
+
   # This is the standard format for flake.nix.
   # `inputs` are the dependencies of the flake,
   # and `outputs` function will return all the build results of the flake.
