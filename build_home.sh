15b161788bbc7fb9cf84796cc59ab0a40f24ea04
Michal Parusinski <michal@parusinski.me>
2023-12-03 20:36:27 +0100

    Cleaning up and adding missing file

---
+++ build_home.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+APPLICATION=$(basename "$0")
+usage () {
+    echo "$APPLICATION [-h/--help] -- Build the Home Manager configuration for machine"
+}
+
+VALID_ARGS=$(getopt -o h --long help -- "$@")
+if [[ $? -ne 0 ]]; then
+    exit 1;
+fi
+
+eval set -- "$VALID_ARGS"
+while [ : ]; do
+    case "$1" in
+        -h | --help)
+            usage
+            exit 0
+            shift
+            ;;
+        --) shift;
+            break
+            ;;    
+    esac
+done
+            
+CONFIG="$(whoami)@$(hostname)"
+echo "Building configuration for user $CONFIG"
+home-manager switch --flake ./#$CONFIG --show-trace
