8afe7280f7f0fb94246d946db56dd0a4d9bdd3de
Michal Parusinski <michal@parusinski.me>
2023-12-11 15:13:18 +0100

    Adding gnome-tweaks

--- system/thor/configuration.nix
+++ system/thor/configuration.nix
@@ -62,6 +62,7 @@
   environment.systemPackages = with pkgs; [
     vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     cifs-utils
+    gnome3.gnome-tweaks
   ];
   services.gvfs.enable = true;
   services.tumbler.enable = true;
