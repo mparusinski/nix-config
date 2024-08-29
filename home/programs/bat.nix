d05bf3e983558fb339efd58dde97febcb5f01364
Michal Parusinski <michal@parusinski.me>
2024-08-29 14:11:26 +0000

    --bat

---
+++ home/programs/bat.nix
@@ -0,0 +1,16 @@
+
+{ config, lib, pkgs, ...}:
+
+{
+  programs.git = {
+    enable = true;
+    lfs.enable = true;
+    userName = "Michal Parusinski";
+    userEmail = "michal@parusinski.me";
+    extraConfig = {
+      init.defaultBranch = "main";
+      credential.helper = "store";
+      pull.rebase = false;
+    };
+  };
+}
