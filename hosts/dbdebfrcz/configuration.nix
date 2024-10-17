15dde9974d254a9d002ee8d62b2b0d5f19e7864f
Michal Parusinski <michal@parusinski.me>
2024-10-17 19:40:03 +0200

    dbdebfrcz : prom + postgresql

--- hosts/dbdebfrcz/configuration.nix
+++ hosts/dbdebfrcz/configuration.nix
@@ -37,5 +37,48 @@
 
   services.tailscale.enable = true;
 
+  # PostgreSQL
+  services.postgresql = {
+    enable = true;
+    ensureDatabases = [ "pgmetricsdb" ];
+    enableTCPIP = true;
+    authentication = pkgs.lib.mkOverride 10 ''
+      #type database  DBuser  auth-method
+      local all       all     trust
+      host  pgmetricsdb  pgmetricsuser     0.0.0.0/32 scram-sha-256
+      host  pgmetricsdb  pgmetricsuser     ::/128 scram-sha-256
+    '';
+  };
+
+  # Prometheus
+  services.prometheus = {
+    enable = true;
+    globalConfig.scrape_interval = "1m"; # "1m"
+    scrapeConfigs = [
+      {
+        job_name = "node_dbdebfrcz";
+        static_configs = [{
+          targets = [ "localhost:${toString config.services.prometheus.exporters.node.port}" ];
+        }];
+      }
+      {
+        job_name = "node_homeassistant";
+        metrics_path = "/metrics";
+        static_configs = [{
+          targets = [ "homeassistant:9100" ];
+        }];
+      }
+      {
+        job_name = "home_mols";
+        metrics_path = "/api/prometheus";
+        bearer_token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJkZmU5MDhhYzEwZDI0ZmQ4ODE0MWYxMTA5ODM4OWMxNCIsImlhdCI6MTcyODc1ODMzNywiZXhwIjoyMDQ0MTE4MzM3fQ.gSd9LfPm2QultokAAcALIeZTLcgbAZU6uy0U-Z05_LA";
+        # bearer_token_file = "/etc/secrets/homeassistant_bearer_token";
+        static_configs = [{
+          targets = [ "homeassistant:8123" ];
+        }];
+      }
+    ];
+  };
+
   system.stateVersion = "24.05";
 }
