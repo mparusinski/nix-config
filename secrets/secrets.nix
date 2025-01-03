2e0ea08e92b3d863fb827075ccb28a86f0790f22
Michal Parusinski <michal@parusinski.me>
2025-01-03 19:30:28 +0100

    --update to secret tokens to use proper ssh keys after reinstall

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,13 +1,13 @@
 let
   # SSH public keys of the users who will be adding secrets through agenix
-  mparus = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJCizmOo5KevfHd6pqwxVjgvVYWv4Az5TbAclvuhF2AC";
+  mparus = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGSZMXih0bhOeWWZ/scrXJsaxwxVqPqBCvML1OCPhMw/";
   users = [ mparus ];
 
   # SSH public keys of the systems who will need to access the secrets
-  dbdebfrcz = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDA1+TbC/tXsVAKUjSzipoC0ibOgSWuNvzVdb8Xxwi0T";
-  systems = [ dbdebfrcz ];
+  vps-nix-vpsfreecz = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGog+SaFVPhrq36aO9A7rC5Y/DUzSAdLcBjUdyZDdg1R";
+  systems = [ vps-nix-vpsfreecz ];
 in
-  {
-    # Specify for each secret who has access to it
-    "hassBearerToken.age".publicKeys = users ++ systems;
-  }
+{
+  # Specify for each secret who has access to it
+  "hassBearerToken.age".publicKeys = users ++ systems;
+}
