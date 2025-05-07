70b235252ce03b05992fc8ba89382c1694f4869b
Michal Parusinski <michal@parusinski.me>
2025-05-07 19:52:57 +0200

    --applying black metal theme to kitty

--- modules/home-manager/kitty.nix
+++ modules/home-manager/kitty.nix
@@ -6,23 +6,27 @@
 }:
 
 {
+  home.file.".config/kitty/current-theme.conf" = {
+    source = ./Black_Metal__Mayhem.conf;
+    recursive = true;
+  };
+
   programs.kitty = {
     enable = true;
     settings.enable_audio_bell = false;
+    settings.background_opacity = "0.9";
+    settings.visual_bell_duration = "0.5";
     settings.confirm_os_window_close = 0;
     settings.font_size = "12.0";
-    settings.font_family = "Fira Code";
+    settings.font_family = "iosevka";
     settings.allow_remote_control = true;
     settings.listen_on = "unix:/tmp/mykitty";
     settings.enabled_layouts = "splits";
-    theme = "Dracula";
+    extraConfig = ''
+      include ~/.config/kitty/current-theme.conf
+    '';
   };
-  home.packages = [
-    (pkgs.nerdfonts.override {
-      fonts = [
-        "FiraCode"
-        "DroidSansMono"
-      ];
-    })
+  home.packages = with pkgs; [
+    iosevka
   ];
 }
