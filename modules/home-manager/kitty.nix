f1adc7c82e873c8d789b51519d76757a988e7fbe
Michal Parusinski <michal@parusinski.me>
2025-07-27 13:38:32 +0200

    enabling opacity

--- modules/home-manager/kitty.nix
+++ modules/home-manager/kitty.nix
@@ -8,11 +8,10 @@
 {
   programs.kitty = {
     enable = true;
-    settings.transparency = "0.9";
     settings.enable_audio_bell = false;
     settings.visual_bell_duration = "0.5";
     settings.confirm_os_window_close = 0;
-    settings.opacity = "0.9";
+    settings.background_opacity = "0.9";
     settings.font_size = "12.0";
     settings.font_family = "iosevka";
     settings.allow_remote_control = true;
